428ee56a4211af14bfcb4e827877ad79ef85bbaf
Osmolovskiy Yuriy <j-thrash@ya.ru>
2022-07-10 17:16:56 +0300

    Доделать

--- src/Processing.java
+++ src/Processing.java
@@ -1,11 +1,14 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Processing {
 
     static String fileName = "Employees.txt";
     static FileInputStream fis;
+
     static {
         try {
             fis = new FileInputStream(fileName);
@@ -19,29 +22,29 @@ public class Processing {
 
     static ArrayList<Workers> employees = new ArrayList<>();
 
-    public void start () throws IOException {
+    public void start() throws IOException {
         choice();
         enter(choiceBr.next());
     }
 
-    private void enter (String string) throws IOException {
+    private void enter(String string) throws IOException {
 
         switch (string) {
             case "1":
-                employees.add(new Workers (br.readLine(), br.readLine(), br.readLine()));
+                employees.add(new Workers(br.readLine(), br.readLine(), br.readLine()));
                 print();
                 break;
             case "2":
-                employees.add(new Workers.Manager (br.readLine(), br.readLine(), br.readLine()));
+                employees.add(new Workers.Manager(br.readLine(), br.readLine(), br.readLine()));
                 print();
                 break;
             case "3":
-                employees.add(new Workers.Others (br.readLine(), br.readLine(), br.readLine()));
+                employees.add(new Workers.Others(br.readLine(), br.readLine(), br.readLine()));
                 print();
                 break;
             case "4":
-                System.out.println("---Введите номер по порядку сотрудника, которого хотите удалить---"+ "\n");
-                employees.remove (choiceBr.nextInt() - 1);
+                System.out.println("---Введите номер по порядку сотрудника, которого хотите удалить---" + "\n");
+                employees.remove(choiceBr.nextInt() - 1);
                 print();
                 break;
             case "5":
@@ -50,33 +53,63 @@ public class Processing {
 
                 int j = (choiceBr.nextInt() - 1);
                 int k = (choiceBr.nextInt());
-                switch (k){
+                switch (k) {
                     case 1:
-                        employees.set (j, new Workers(" ", " ", " "));
+                        employees.set(j, new Workers(" ", " ", " "));
+                        break;
                     case 2:
-                        employees.set (j, new Workers.Manager(" ", " ", " "));
+                        employees.set(j, new Workers.Manager(" ", " ", " "));
+                        break;
                     case 3:
-                        employees.set (j, new Workers.Others (" ", " ", " "));
+                        employees.set(j, new Workers.Others(" ", " ", " "));
+                        break;
                 }
-                print();
-                break;
             case "6":
-                Workers.Manager.addSub((choiceBr.nextInt() - 1), );
+                Workers.Manager.addSub((choiceBr.nextInt() - 1), new Workers(" ", " ", " "));
+
+            case "7":
+                System.out.println("Введите номер желаемой сортировки: " +
+                        "\n" + "-- 1 -- --- по ФИО" + "\n" + "-- 2 -- --- дате принятия на работу" + "\n");
+
+                int l = (choiceBr.nextInt());
+                switch (l) {
+                    case 1:
+                        Comparator<Workers> compareByFIO = new Comparator<Workers>() {
+                            @Override
+                            public int compare(Workers o1, Workers o2) {
+                                return o1.fio.compareTo(o2.fio);
+                            }
+                        };
+                        Collections.sort(employees, compareByFIO);
+                        print();
+                        break;
+                    case 2:
+                        Comparator<Workers> compareByDate = new Comparator<Workers>() {
+                            @Override
+                            public int compare(Workers o1, Workers o2) {
+                                return o1.dateWork.compareTo(o2.dateWork);
+                            }
+                        };
+                        Collections.sort(employees, compareByDate);
+                        print();
+                        break;
+                }
+            }
+            enter(choiceBr.next());
+
+    }
+
+        private void choice () {
+            System.out.println("Что Вы хотите сделать? \n" + "1. Добавить работника \n" + "2. Добавить Менеджера \n" +
+                    "3. Добавить другого сотрудника \n" + "4. Удалить сотрудника \n" + "5. Изменить тип сотрудника \n"
+                    + "6. Привязать сотрудника к менеджеру \n" + "7. Сортировать список по фамилиям, датам принятия на работу \n");
         }
-        enter(choiceBr.next());
-    }
 
-    private void choice(){
-        System.out.println("Что Вы хотите сделать? \n" + "1. Добавить работника \n" + "2. Добавить Менеджера \n" +
-                "3. Добавить другого сотрудника \n" + "4. Удалить сотрудника \n" + "5. Изменить тип сотрудника \n"
-                + "6. Привязать сотрудника к менеджеру \n" + "7. Сортировать список по фамилиям, датам принятия на работу \n");
-    }
-
-    public static void print() {
-        System.out.println("Список сотрудников: ");
-        for (Workers a : employees) {
-            System.out.println(a);
+        public static void print () {
+            System.out.println("Список сотрудников: ");
+            for (Workers a : employees) {
+                System.out.println(a);
+            }
         }
-    }
 
-}
+    }
