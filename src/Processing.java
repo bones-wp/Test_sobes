d46b316e01576fb683fff752917b45cdc5ec0f43
Osmolovskiy Yuriy <j-thrash@ya.ru>
2022-07-10 20:14:10 +0300

    Доделать

--- src/Processing.java
+++ src/Processing.java
@@ -30,24 +30,24 @@ public class Processing {
     private void enter(String string) throws IOException {
 
         switch (string) {
-            case "1":
+            case "addWorker":
                 employees.add(new Workers(br.readLine(), br.readLine(), br.readLine()));
                 print();
                 break;
-            case "2":
+            case "addManager":
                 employees.add(new Workers.Manager(br.readLine(), br.readLine(), br.readLine()));
                 print();
                 break;
-            case "3":
+            case "addOther":
                 employees.add(new Workers.Others(br.readLine(), br.readLine(), br.readLine()));
                 print();
                 break;
-            case "4":
+            case "delWorker":
                 System.out.println("---Введите номер по порядку сотрудника, которого хотите удалить---" + "\n");
                 employees.remove(choiceBr.nextInt() - 1);
                 print();
                 break;
-            case "5":
+            case "replace":
                 System.out.println("Введите номер по порядку сотрудника, тип которого хотите изменить. Затем введите желаемый новый тип: " +
                         "\n" + "-- 1 -- --- Рабочий" + "\n" + "-- 2 -- --- Менеджер" + "\n" + "-- 3 -- --- Другое" + "\n");
 
@@ -64,10 +64,13 @@ public class Processing {
                         employees.set(j, new Workers.Others(" ", " ", " "));
                         break;
                 }
-            case "6":
-                Workers.Manager.addSub((choiceBr.nextInt() - 1), new Workers(" ", " ", " "));
+            case "toManager":
+                int f = (choiceBr.nextInt() - 1);
+                employees.addAll(f, Workers.Manager.subordinates);
+                printSub();
+                break;
 
-            case "7":
+            case "sort":
                 System.out.println("Введите номер желаемой сортировки: " +
                         "\n" + "-- 1 -- --- по ФИО" + "\n" + "-- 2 -- --- дате принятия на работу" + "\n");
 
@@ -100,9 +103,9 @@ public class Processing {
     }
 
         private void choice () {
-            System.out.println("Что Вы хотите сделать? \n" + "1. Добавить работника \n" + "2. Добавить Менеджера \n" +
-                    "3. Добавить другого сотрудника \n" + "4. Удалить сотрудника \n" + "5. Изменить тип сотрудника \n"
-                    + "6. Привязать сотрудника к менеджеру \n" + "7. Сортировать список по фамилиям, датам принятия на работу \n");
+            System.out.println("Что Вы хотите сделать? \n" + "Добавить работника -> addWorker \n" + "Добавить Менеджера -> addManager \n" +
+                    "Добавить другого сотрудника -> addOther \n" + "Удалить сотрудника - delWorker \n" + "Изменить тип сотрудника -> replace \n"
+                    + "Привязать сотрудника к менеджеру - toManager\n" + "Сортировать список по фамилиям, датам принятия на работу -> sort \n");
         }
 
         public static void print () {
@@ -111,5 +114,12 @@ public class Processing {
                 System.out.println(a);
             }
         }
-
+        public static void printSub () {
+        System.out.println("Список сотрудников в подчиненнии менеджера: ");
+        for (Workers a : Workers.Manager.subordinates) {
+            System.out.println(a);
+        }
     }
+
+
+}
